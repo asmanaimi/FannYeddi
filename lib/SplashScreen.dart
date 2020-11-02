c9a6af08b2e0232470f58c7fed7eead4b8f27542
yassmin <yassmin7715@gmail.com>
2020-11-02 12:26:26 +0100

    launchscreen

--- lib/SplashScreen.dart
+++ lib/SplashScreen.dart
@@ -5,22 +5,20 @@ import 'package:flutter/material.dart';
 import 'HomeScreen.dart';
 import 'LogInScreen.dart';
 
-class SplashScreen extends StatefulWidget{
+class SplashScreen extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
     return _SplashScreen();
   }
-
 }
 
-class _SplashScreen extends State<SplashScreen>{
+class _SplashScreen extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
       backgroundColor: Color(0xff2E001F),
-
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -31,48 +29,50 @@ class _SplashScreen extends State<SplashScreen>{
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
                     MyImage(),
-                    Text("Scarves Store",style: TextStyle(fontSize: 35
-                        ,fontStyle: FontStyle.italic,color: Color(0xff2E001F)),
+                    Text(
+                      "Scarves Store",
+                      style: TextStyle(
+                          fontSize: 35,
+                          fontStyle: FontStyle.italic,
+                          color: Color(0xff2E001F)),
                     )
                   ],
-                )
-            ),
-
+                )),
             Expanded(
                 flex: 1,
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
                     CircularProgressIndicator(),
-                    SizedBox(height: 20,),
-                    Text("Online store for everyone !"
-                      ,style: TextStyle(
-                          fontStyle: FontStyle.italic
-                          ,fontSize: 18
-                          ,color: Color(0xffffffff)),)
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      "Online store for everyone !",
+                      style: TextStyle(
+                          fontStyle: FontStyle.italic,
+                          fontSize: 18,
+                          color: Color(0xffffffff)),
+                    )
                   ],
-                )
-            ),
+                )),
           ],
         ),
       ),
-
     );
   }
-  FirebaseAuth auth =FirebaseAuth.instance;
-//first we need to navigate to log in screen
-  void NavigateToLogIn(){
-    Timer(Duration(seconds: 5), () async {
-      if(await auth.currentUser()==null) {
-        Navigator.of(context).pushReplacement(
-            MaterialPageRoute(
-                builder: (BuildContext context) => LogInScreen()));
-      }else{
-        currentEmail();
 
+  FirebaseAuth auth = FirebaseAuth.instance;
+//first we need to navigate to log in screen
+  void NavigateToLogIn() {
+    Timer(Duration(seconds: 5), () async {
+      if (await auth.currentUser() == null) {
+        Navigator.of(context).pushReplacement(MaterialPageRoute(
+            builder: (BuildContext context) => LogInScreen()));
+      } else {
+        currentEmail();
       }
     });
-
   }
 
   @override
@@ -83,19 +83,24 @@ class _SplashScreen extends State<SplashScreen>{
 
   Future<void> currentEmail() async {
     await auth.currentUser().then((value) {
-      Navigator.of(context).pushReplacement(
-          MaterialPageRoute(
-              builder: (BuildContext context) => HomeScreen(value.email)));
+      Navigator.of(context).pushReplacement(MaterialPageRoute(
+          builder: (BuildContext context) => HomeScreen(value.email)));
     });
   }
-
 }
 
-class MyImage extends StatelessWidget{
+class MyImage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-   AssetImage image=new AssetImage("images/logo.png");
-    Image logo=new Image(image: image,width: 50,height: 50,);
-    return logo;
+    AssetImage assetImage = AssetImage("images/logo.png");
+    /* Image logo = new Image(
+      image: image,
+      width: 50,
+      height: 50,
+    );*/
+    Image image = Image(image: assetImage);
+    return Container(
+      child: image,
+    );
   }
 }
